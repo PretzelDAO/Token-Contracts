21e520fdb5fd68293101e5ac6e830f0c27a73427
Nick Stracke <nick.stracke@web.de>
2022-05-24 12:21:37 +0200

    add GPS contract

---
+++ contracts/GenesisPretzels/GenesisPretzels.sol
@@ -0,0 +1,92 @@
+pragma solidity ^0.8.4;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GenesisPretzels is ERC721A, Ownable {
+    bool public revealed = false;
+    uint256 public metadataOffset;
+    uint256 public constant PRICE_PER_MINT = 0.1 ether;
+    uint256 public constant MAX_MINT_PER_TX = 5;
+
+    string private constant METADATA_CID = "metadata";
+    string private constant UNREVEALED_CID = "unrevealed";
+    uint256 public constant MAX_SUPPLY = 30;
+
+    constructor() ERC721A("GenesisPretzels", "GPS") {}
+
+    function mint(uint256 quantity) external payable {
+        require(
+            _totalMinted() + quantity <= MAX_SUPPLY,
+            "Trying to mint too many tokens."
+        );
+        require(
+            quantity <= MAX_MINT_PER_TX,
+            "Trying to mint too many tokens in one transaction."
+        );
+        require(
+            msg.value >= quantity * PRICE_PER_MINT,
+            "Not enough ether send to complete purchase"
+        );
+        _safeMint(msg.sender, quantity);
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overriden in child contracts.
+     */
+    function _baseURI() internal pure override returns (string memory) {
+        return string(abi.encodePacked("ipfs://", METADATA_CID, "/"));
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+        if (!revealed) {
+            return string(abi.encodePacked("ipfs://", UNREVEALED_CID));
+        }
+        return string(abi.encodePacked(_baseURI(), tokenToMetadataId(tokenId)));
+    }
+
+    function tokenToMetadataId(uint256 tokenId) public view returns (uint256) {
+        require(revealed, "Metadata must be revealed first.");
+        unchecked {
+            return ((tokenId + metadataOffset) % MAX_SUPPLY);
+        }
+    }
+
+    function reveal() external onlyOwner {
+        require(!revealed, "Can only reveal once.");
+        revealed = true;
+        metadataOffset = getRandomOffset();
+    }
+
+    function getRandomOffset() private view returns (uint256) {
+        uint256 randomWord = uint256(
+            keccak256(
+                abi.encode(
+                    tx.gasprice,
+                    block.number,
+                    block.timestamp,
+                    block.difficulty,
+                    blockhash(block.number - 1),
+                    address(this),
+                    totalSupply()
+                )
+            )
+        );
+        return randomWord % MAX_SUPPLY;
+    }
+
+    function withdraw() external payable onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
